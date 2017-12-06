b1cd73fd7f26d6448f756e5f0179ed01cd7fb7c5
Dave Pagurek <dave@davepagurek.com>
2017-12-06 09:58:58 -0500

    Code to run in console

---
+++ gpm-stats.js
@@ -0,0 +1,55 @@
+// Date is a string like "September 1"
+function collectDataUntil(date) {
+  const content = document.getElementById('main-content');
+
+  function collectUntil(date, callback) {
+    const UPDATE_INTERVAL = 200;
+    const interval = setInterval(() => {
+      const hasDate = h2 => h2.textContent == date;
+      if (Array.prototype.some.call(document.querySelectorAll('h2'), hasDate)) {
+        clearInterval(interval);
+        callback();
+      } else {
+        // Keep scrolling down
+        content.scrollTop = content.scrollHeight;
+      }
+    }, UPDATE_INTERVAL);
+  }
+
+  function processCards() {
+    const data = [];
+    let currentDate = null;
+
+    Array.prototype.forEach.call(document.querySelectorAll('md-card-content'), card => {
+      const date = card.querySelector('.fp-date-block-date');
+      if (date) {
+        const dateText = date.textContent.trim();
+        if (dateText == 'Today') {
+          currentDate = new Date();
+        } else if (dateText == 'Yesterday') {
+          currentDate = new Date(new Date().setDate(new Date().getDate()-1));
+        } else {
+          currentDate = new Date(dateText);
+        }
+        return;
+      }
+
+      const title = card.querySelector('h4 span');
+      const artist = card.querySelector('h4+div > div');
+      if (!title || !artist) return;
+
+      data.push({
+        title: title.textContent.trim(),
+        artist: artist.textContent.trim(),
+        date: currentDate.toString()
+      });
+    });
+
+    return data;
+  }
+
+  collectDataUntil(date, () => {
+    const data = processCards();
+    console.log(JSON.stringify(data));
+  });
+}
